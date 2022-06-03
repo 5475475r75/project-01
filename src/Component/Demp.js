006a6914b97014e2b5e9b79ded69e2ac1bd7f49c
5475475r75 <dhananibrijesh26@gmail.com>
2022-06-03 10:15:20 +0530

    27_05

---
+++ src/Component/Demp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Demo extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Brijesh</h2>
+            </div>
+        );
+    }
+}
+
+export default Demo;
